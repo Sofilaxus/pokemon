2aee3f2f4b53a9fba3ead4bac477307ac37bb4a5
Sofia Serine Mikkelsen <sofia.mikkelsen@arribatec.com>
2025-06-13 09:12:06 +0200

    Created types and connected the pokemon json to the table

--- src/App.js
+++ src/App.js
@@ -1,32 +1,48 @@
-import './App.css';
+import "./App.css";
+import PropTypes from "prop-types";
+import pokemon from "./pokemon.json";
+
+const PokemonRow = ({ pokemon }) => (
+    <tr>
+        <td>{pokemon.name.english}</td>
+        <td>{pokemon.type.join(", ")}</td>
+    </tr>
+);
+
+PokemonRow.propTypes = {
+  pokemon: PropTypes.shape({
+    name: PropTypes.shape({
+      english: PropTypes.string
+    }),
+    type: PropTypes.arrayOf(PropTypes.string)
+  }),
+}
 
 function App() {
-  return (
-    <div 
-      style={{
-        margin: "auto",
-        width: 800,
-        paddingTop: "1rem",
-      }}
-    >
-      <h1 className="title">Pokemon Search</h1>
-      <table width="100%">
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Type</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-          <td>Bulbasaur</td>
-          <td>Grass, Poison</td>
-        </tr>
-        </tbody>
-      
-      </table>
-    </div>
-  );
+    return (
+        <div
+            style={{
+                margin: "auto",
+                width: 800,
+                paddingTop: "1rem",
+            }}
+        >
+            <h1 className="title">Pokemon Search</h1>
+            <table width="100%">
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Type</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {pokemon.slice(0, 20).map((pokemon) => (
+                        <PokemonRow pokemon={pokemon} key={pokemon.id} />
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
 }
 
 export default App;
